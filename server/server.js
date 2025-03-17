4b21a853d5623f772e00a22e95fb61041afe3713
Teddy <teddykoeh@gmail.com>
2025-03-17 01:53:35 +0100

    Mise à jour

--- server/server.js
+++ server/server.js
@@ -2,22 +2,47 @@ const express = require('express')
 const http = require('http')
 const { Server } = require('socket.io')
 const cors = require('cors')
+
 const app = express()
-app.use(cors())
+
+const allowedOrigins = ['http://vivachat.ct.ws', 'http://localhost:3000', 'http://127.0.0.1:3000', 'null']
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      return callback(null, true)
+    } else {
+      return callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true
+}))
+
 const server = http.createServer(app)
-const io = new Server(server, { cors: { origin: '*' } })
+
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigins,
+    credentials: true
+  }
+})
+
 let users = {}
+
 io.on('connection', socket => {
   socket.on('setUsername', username => {
     if (!users[socket.id]) users[socket.id] = { id: socket.id }
     users[socket.id].username = username
     io.emit('onlineUsers', Object.values(users))
   })
+
   socket.on('setAvatar', avatar => {
     if (!users[socket.id]) users[socket.id] = { id: socket.id }
     users[socket.id].avatar = avatar
     io.emit('onlineUsers', Object.values(users))
   })
+
   socket.on('sendMessage', data => {
     if (users[data.to]) {
       io.to(data.to).emit('receiveMessage', {
@@ -26,10 +51,13 @@ io.on('connection', socket => {
       })
     }
   })
+
   socket.on('disconnect', () => {
     delete users[socket.id]
     io.emit('onlineUsers', Object.values(users))
   })
 })
-server.listen(22453, () => {})
-console.log("listening on 22453")
+
+server.listen(22453, () => {
+  console.log("listening on 22453")
+})
